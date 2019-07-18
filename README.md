8d5f0e737fa2bcf9ba2ff4fdd50db5033a2f9175
Dirk <dirkstals@gmail.com>
2019-07-18 16:43:13 +0200

    Add versioned clientlibs

--- README.md
+++ README.md
@@ -1,1 +1,42 @@
 # Webcomponent to AEM component
+
+> Transpile webcomponents to AEM (Adobe Experience Manager) components.
+
+## Introduction
+
+This script will search for webcomponents that are defined in the customElement registry. Then transpiles these components to AEM components.
+
+## Requirements
+
+### Node
+
+Node 10 is required.
+
+### Webcomponents
+
+Either provide a packaged module where webcomponents or custom elements are defined. Or provide a file directly.
+
+## Installing
+
+Install all dependencies required for the script to run.
+
+```
+npm install
+```
+
+## Run the script
+
+Start the script by providing the following command.
+
+```
+npm start
+```
+
+It will ask the following questions:
+
+```
+Enter a module or file you wish to transpile:
+Enter a component group for your library:
+Do you wish to use versioned clientlibs?
+Enter an AEM app directory (optionally):
+```
--- index.js
+++ index.js
@@ -1,17 +1,23 @@
-require("@skatejs/ssr/register");
-const mkdirp = require('mkdirp');
-const inquirer = require('inquirer');
-const { push } = require('aemsync');
 const fs = require('fs');
 const { resolve } = require('path');
 const cp = require('child_process');
 
-const templateDirectory = resolve(__dirname, 'templates');
+const puppeteer = require('puppeteer');
+const webpack = require('webpack');
+const mkdirp = require('mkdirp');
+const inquirer = require('inquirer');
+const { push } = require('aemsync');
+
 const buildDirectory = resolve(__dirname, 'dist');
+const templateDirectory = resolve(__dirname, 'templates');
 const contentFile = fs.readFileSync(`${templateDirectory}/.content.xml`, 'utf8');
-const componentContentDialogFile = fs.readFileSync(`${templateDirectory}/component/_cq_dialog/.content.xml`, 'utf8');
-const componentContentFile = fs.readFileSync(`${templateDirectory}/component/.content.xml`, 'utf8');
-const componentHtmlFile = fs.readFileSync(`${templateDirectory}/component/component.html`, 'utf8');
+const versionedComponentContentFile = fs.readFileSync(`${templateDirectory}/component/.content.xml`, 'utf8');
+const versionedContentFile = fs.readFileSync(`${templateDirectory}/component/v1/.content.xml`, 'utf8');
+const componentContentDialogFile = fs.readFileSync(`${templateDirectory}/component/v1/component/_cq_dialog/.content.xml`, 'utf8');
+const componentContentFile = fs.readFileSync(`${templateDirectory}/component/v1/component/.content.xml`, 'utf8');
+const componentHtmlFile = fs.readFileSync(`${templateDirectory}/component/v1/component/component.html`, 'utf8');
+
+// headless browser
 
 const UpperCase = c => c.charAt(0).toUpperCase() + c.slice(1);
 const req = async module => {
@@ -23,10 +29,10 @@ const req = async module => {
   }
 
   try {
-    return require(module);
+    return require.resolve(module);
   } catch (e) {
     console.log(`Could not require "${module}".`);
-    process.exit(1);
+    process.exit(0);
   }
 }
 
@@ -34,12 +40,17 @@ const questions = [
   {
     type: 'input',
     name: 'module',
-    message: 'Enter a module you wish to transpile:',
+    message: 'Enter a module or file you wish to transpile:',
   },
   {
     type: 'input',
     name: 'group',
     message: 'Enter a component group for your library:',
+  },
+  {
+    type: 'confirm',
+    name: 'versioned',
+    message: 'Do you wish to use versioned clientlibs?',
   },
   {
     type: 'input',
@@ -48,70 +59,112 @@ const questions = [
   },
 ];
 
-inquirer.prompt(questions).then(async ({ module, group, project }) => {
+inquirer.prompt(questions).then(async ({ module, group, versioned, project }) => {
 
-  await req(module);
+  const path = await req(module);
 
-  try {
-    const directory = (project !== ''
-      ? `${buildDirectory}/jcr_root/apps/${project}/components/content/${module}`
-      : `${buildDirectory}/${module}`
-    );
+  // Bundle the script with Webpack
+  await new Promise(resolve => webpack({ entry: path }, resolve));
 
-    mkdirp.sync(directory);
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
 
-    fs.writeFileSync(`${directory}/.content.xml`, contentFile);
+  // Magic
+  await page.addScriptTag({ content: `
+    const origDefine = window.customElements.define;
+    window.customElementList = {};
+    window.customElements.define = (n, c, o) => window.customElementList[n] = c.observedAttributes;` });
+  await page.addScriptTag({ path: `${buildDirectory}/main.js` });
 
-    for (customElement in customElements.registry) {
-      console.log(`Creating ${customElement}.`);
+  // Extract the results from the page
+  const customElements = await page.evaluate(() => window.customElementList);
+  browser.close();
 
-      const key = customElement;
-      const name = (a => { return a.shift(), a.join('-')})(key.split('-'));
-      const attributes = customElements.registry[key].observedAttributes;
+  fs.unlinkSync(`${buildDirectory}/main.js`);
 
-      // Create component directory
-      mkdirp.sync(`${directory}/${name}`);
-
-      // # generate .content.xml file
-      fs.writeFileSync(`${directory}/${name}/.content.xml`,
-        componentContentFile
-          .replace(/\{title\}/g, UpperCase(name))
-          .replace(/\{group\}/g, group));
-
-      // # generate component.html file
-      fs.writeFileSync(
-        `${directory}/${name}/${name}.html`,
-        componentHtmlFile
-          .replace(/\{tag\}/g, key)
-          .replace(/\{attributes\}/g, attributes.map(attr => `${attr}="\$\{properties.${attr}\}"`).join(`
-  `))
+  if (Object.keys(customElements).length > 0) {
+    try {
+      const directory = (project !== ''
+        ? `${buildDirectory}/jcr_root/apps/${project}/components/content/${module}`
+        : `${buildDirectory}/${module}`
       );
 
-      mkdirp.sync(`${directory}/${name}/_cq_dialog`);
+      mkdirp.sync(directory);
 
-      // # generate _cq_dialog/.content.xml file
-      fs.writeFileSync(
-        `${directory}/${name}/_cq_dialog/.content.xml`,
-        componentContentDialogFile
-          .replace(/\{title\}/g, UpperCase(name))
-          .replace(/\{attributes\}/g, attributes.map(attr => `<${attr}
-                                                jcr:primaryType="nt:unstructured"
-                                                sling:resourceType="granite/ui/components/coral/foundation/form/textfield"
-                                                fieldLabel="${UpperCase(attr)}"
-                                                name="./${attr}"/>`).join(`
-                                            `))
-      );
+      fs.writeFileSync(`${directory}/.content.xml`, contentFile);
+
+      for (customElement in customElements) {
+        console.log(`Creating ${customElement}.`);
+
+        const key = customElement;
+        const name = (a => { return a.shift(), a.join('-')})(key.split('-'));
+        const attributes = customElements[customElement];
+
+        let componentDirectory = `${directory}/${name}`;
+
+        // Create component directory
+        mkdirp.sync(componentDirectory);
+
+        if (versioned) {
+          fs.writeFileSync(`${componentDirectory}/.content.xml`, versionedComponentContentFile);
+
+          mkdirp.sync(`${componentDirectory}/v1`);
+
+          fs.writeFileSync(`${componentDirectory}/v1/.content.xml`,
+            versionedContentFile.replace(/\{component\}/g, name)
+          );
+
+          componentDirectory = `${directory}/${name}/v1/${name}`;
+
+          mkdirp.sync(componentDirectory);
+        }
+
+        fs.writeFileSync(`${componentDirectory}/.content.xml`, contentFile);
+
+
+        // # generate .content.xml file
+        fs.writeFileSync(`${componentDirectory}/.content.xml`,
+          componentContentFile
+            .replace(/\{title\}/g, UpperCase(name))
+            .replace(/\{group\}/g, group));
+
+        // # generate component.html file
+        fs.writeFileSync(
+          `${componentDirectory}/${name}.html`,
+          componentHtmlFile
+            .replace(/\{tag\}/g, key)
+            .replace(/\{attributes\}/g, attributes.map(attr => `${attr}="\$\{properties.${attr}\}"`).join(`
+    `))
+        );
+
+        mkdirp.sync(`${componentDirectory}/_cq_dialog`);
+
+        // # generate _cq_dialog/.content.xml file
+        fs.writeFileSync(
+          `${componentDirectory}/_cq_dialog/.content.xml`,
+          componentContentDialogFile
+            .replace(/\{title\}/g, UpperCase(name))
+            .replace(/\{attributes\}/g, attributes.map(attr => `<${attr}
+                                                  jcr:primaryType="nt:unstructured"
+                                                  sling:resourceType="granite/ui/components/coral/foundation/form/textfield"
+                                                  fieldLabel="${UpperCase(attr)}"
+                                                  name="./${attr}"/>`).join(`
+                                              `))
+        );
+      }
+
+      if (project) {
+        await push(directory, {
+          targets: [
+            'http://admin:admin@localhost:4502'
+          ]
+        });
+      }
+
+    } catch(e) {
+      console.log(e);
     }
-
-    if (project) {
-      await push(directory, {
-        targets: [
-          'http://admin:admin@localhost:4502'
-        ]
-      });
-    }
-
-  } catch(e) {
-    console.log(e);
+  } else {
+    console.log(`No custom elements found in ${module}.`);
   }
 });
